cd68755543033af0171ec5c60e398b557aa91058
LuisZeballos <lazeballos@miuandes.cl>
2020-03-22 20:27:02 -0300

    First Commit

---
+++ ConsoleApp1/ConsoleApp1/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public static int GetLanzar()
+        {
+            Random rnd = new Random();
+            int numero = rnd.Next(0, 3);
+            return numero;
+
+        }
+        public Person(string aFirstName, string aLastName)
+        {
+            FirstName = aFirstName;
+            LastName = aLastName;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Person P1 = new Person("Bob","Kunga");
+
+            
+            Console.WriteLine(Person.GetLanzar());
+            Console.WriteLine(P1.FirstName);
+            Console.WriteLine(P1.LastName);
+
+            Console.ReadLine();
+        }
+    }
+}
